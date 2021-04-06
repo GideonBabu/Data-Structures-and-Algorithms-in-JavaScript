cff4fe8f9aea874f3291d0dacee04efbfcbc1908
Gideon Babu <gideonvbabu@gmail.com>
2021-04-06 09:51:01 +0200

    add algorithm to divide two integers without using multiplication, division and mod operator - another approach

--- Algorithms/29_Divide_Two_Integers.js
+++ Algorithms/29_Divide_Two_Integers.js
@@ -1,4 +1,61 @@
 /**
+ * time: O(log ^ 2 * n) where n is the dividend in the worst case when divisor is 1
+ * space: O(1)
+ */
+var divide = function(dividend, divisor) {
+
+    // Special case: overflow.
+    if (dividend == -Math.pow(2, 31) && divisor == -1) {
+        return Math.pow(2,31) - 1;
+    }
+
+    /* We need to convert both numbers to negatives
+     * for the reasons explained above.
+     * Also, we count the number of negatives signs. */
+    let negatives = 2;
+    if (dividend > 0) {
+        negatives--;
+        dividend = -dividend;
+    }
+    if (divisor > 0) {
+        negatives--;
+        divisor = -divisor;
+    }
+
+    /* Count how many times the divisor has to be added
+     * to get the dividend. This is the quotient. */
+    let quotient = 0;
+    
+    const HALF_INT_MIN = -Math.floor(Math.pow(2, 31) / 2);
+    
+    while (dividend <= divisor) {
+        
+        let powerOfTwo = -1;
+        
+        let value = divisor;
+        while (value >= HALF_INT_MIN && value + value >= dividend) {
+            value += value;
+            powerOfTwo += powerOfTwo;
+        }
+        
+        
+        quotient += powerOfTwo;
+        dividend -= value;
+    }
+
+    /* If there was originally one negative sign, then
+     * the quotient remains negative. Otherwise, switch
+     * it to positive. */
+    if (negatives != 1) {
+        quotient = -quotient;
+    }
+    return quotient;
+}
+
+
+
+
+/**
  * time: O(n) where n is the dividend in the worst case when divisor is 1
  * space: O(1)
  */
